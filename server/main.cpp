300bc4a6c6b1b4283b0de9846ce0e6c2d0e6ae43
Santiago Jorda <sjorda@fi.uba.ar>
2024-10-22 23:57:17 -0300

    el dipa common files

--- server/main.cpp
+++ server/main.cpp
@@ -1,2 +1,36 @@
+#include <exception>
+#include <iostream>
 
-int main() { return 0; }
+#include "server.h"
+
+#define ERROR -1
+#define SUCCESS 0
+#define BAD_ARGUMENTS 1
+#define ARGS_EXPECTED 2
+
+int main(int argc, char* argv[]) {
+
+    try {
+        Server server;
+        server.run();
+        return SUCCESS;
+
+
+        // if (argc == ARGS_EXPECTED) {
+            // char* servname = argv[1];
+            // Server server;
+            // server.run();
+            // return ...
+        // } else {
+            // std::cerr << "Bad call " << argv[0] << " <servname>" << std::endl;
+            // return BAD_ARGUMENTS;
+        // }
+
+    } catch (const std::exception& error) {
+        std::cerr << "Server error: " << error.what() << std::endl;
+        return ERROR;
+    } catch (...) {
+        std::cerr << "Server error. There is not information." << std::endl;
+        return ERROR;
+    }
+}
