26612f98c1a544d6af67415b1193a47e993e3c5b
Felipe-Druk <fedalto@fi.uba.ar>
2024-11-08 17:49:27 -0300

    Recuperamos constructor doble y ahora el cliente cierra si cierra el drawr (mas segurirad )

--- client/client.cpp
+++ client/client.cpp
@@ -18,27 +18,30 @@ Client::Client(const std::string& hostname, const std::string& servname):
     this->protocol.send_init(ONE_PLAYER);
 }
 
-/*
+
 Client::Client(const std::string& hostname, const std::string& servname, int N_players):
-        hostname(hostname),
-        servicename(servname),
         skt(hostname.c_str(), servname.c_str()),
         protocol(skt),
-        drawer() {
-    this->protocol.send_init(N_players > MAX_PLAYER ? MAX_PLAYER : N_players);
-}*/
+        commands(),
+        positions(),
+        receiver(protocol, positions),
+        sender(protocol, commands),
+        drawer(commands, positions) {
 
+    this->protocol.send_init(N_players > MAX_PLAYER ? MAX_PLAYER : N_players);
+}
 
 void Client::run() {
     // recibo posiciones iniciales
     // std::vector<Coordinate> coordenadas;
     // receiver.receiver_players(coordenadas);
 
-    // while (true) // despues cambiarlo
-    //{
-
     // Starteo los receiver y sender
     receiver.start();
     sender.start();
     drawer.run();
+
+    // drawer termino
+    this->skt.shutdown(2);
+    this->commands.close();
 }
