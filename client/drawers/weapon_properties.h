71c6a1789a28fe78b90af662ed0ad842b43784f4
Alexia Aroa <aaroa@fi.uba.ar>
2024-11-18 14:03:10 -0300

    fix data cowboy gun for drawers

--- client/drawers/weapon_properties.h
+++ client/drawers/weapon_properties.h
@@ -23,11 +23,9 @@ static std::map<WeaponTextureID, weapon_properties_t> weapon_properties = {
         {WeaponTextureID::LASER_RIFLE, {DATA_PATH "/DuckGame-Laser.png", 1, 97, 32, 32}},
         {WeaponTextureID::AK_47, {DATA_PATH "/DuckGame-MachineGuns.png", 1, 19, 32, 32}},
         {WeaponTextureID::DUELING_GUN, {DATA_PATH "/DuckGame-Pistol.png", 35, 68, 18, 10}},
-        {WeaponTextureID::COWBOY_GUN,
-         {DATA_PATH "/DuckGame-Pistol.png", 0, 0, 0,
-          0}},  // Sin coordenadas, me falta encontrar esta :(
+        {WeaponTextureID::COWBOY_GUN, {DATA_PATH "/DuckGame-Pistol.png", 1, 20, 22, 11}},
         {WeaponTextureID::MAGNUM, {DATA_PATH "/DuckGame-Pistol.png", 1, 47, 32, 32}},
-        {WeaponTextureID::SHOTGUN, {DATA_PATH "/DuckGame-Props.png", 60, 114, 41, 11}},
+        {WeaponTextureID::SHOTGUN, {DATA_PATH "/DuckGame-MoreWeapons.png", 60, 114, 41, 11}},
         {WeaponTextureID::SNIPER, {DATA_PATH "/DuckGame-MoreWeapons.png", 36, 237, 33, 9}}};
 
 class WeaponConfig {
