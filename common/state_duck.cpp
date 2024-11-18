f71bcc0a5a95ff9121f384da22fc03978e98f643
Santiago Jorda <sjorda@fi.uba.ar>
2024-11-18 17:21:23 -0300

    ticks and frame_diuration

--- common/state_duck.cpp
+++ common/state_duck.cpp
@@ -1,6 +1,8 @@
 #include "state_duck.h"
 
+// se agrego la cantidad de ticks por frame
+
 std::unordered_map<DuckState, DuckStateInfo_t> duck_state_frames = {
-        {IS_JUMPING, {6}},   {IS_RUNNING, {6}}, {IS_CROUCHING, {1}},
-        {IS_RECOILING, {3}}, {IS_FALLING, {1}}, {IS_SLIPPING, {2}},
-        {IS_PLANING, {6}},   {IS_IDLE, {1}},    {IS_DEAD, {1}}};
+        {IS_JUMPING, {6, 3}},   {IS_RUNNING, {6, 1}}, {IS_CROUCHING, {1, 1}},
+        {IS_RECOILING, {3, 3}}, {IS_FALLING, {1, 3}}, {IS_SLIPPING, {2, 1}},
+        {IS_PLANING, {6, 3}},   {IS_IDLE, {1, 1}},    {IS_DEAD, {1, 1}}};
