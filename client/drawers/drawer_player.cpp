d714abce0f5125fd0b8dfcd551e79eb6ec63e22f
Alexia Aroa <aaroa@fi.uba.ar>
2024-11-21 22:42:35 -0300

    Refactor drawer player

--- client/drawers/drawer_player.cpp
+++ client/drawers/drawer_player.cpp
@@ -50,20 +50,32 @@ void DrawerPlayer::draw(SDL2pp::Renderer& renderer, const player_t& player) {
     case DuckStateType::IS_FALLING:
         
         break;
-    case DuckStateType::IS_JUMPING:
+    case DuckStateType::IS_JUMPING: // saltando
         std::cout << "frame: " << frame << "\n";
-        
-        /*
+        int x = 0;
+        int y = 0;
+        if (frame == 6) { // utilizo la primera fila 
+            x = DUCK_IDLE_X + (SIZE_DUCK_SPRITE * 0);
+            y = DUCK_IDLE_Y + (SIZE_DUCK_SPRITE * 0);
+        } else { // utilizo la segunda fila
+            x = DUCK_IDLE_X + (SIZE_DUCK_SPRITE * frame);
+            y = DUCK_IDLE_Y + (SIZE_DUCK_SPRITE * 1);
+        }
         renderer.Copy(this->texture,
-                      SDL2pp::Rect(DUCK_IDLE_X + (SIZE_DUCK_SPRITE * DUCK_SPRITE_CROUCHING_X), 
-                                    DUCK_IDLE_Y + (SIZE_DUCK_SPRITE * DUCK_SPRITE_CROUCHING_Y), 
+                      SDL2pp::Rect(x, 
+                                    y, 
                                     SIZE_DUCK_SPRITE, SIZE_DUCK_SPRITE),
                       SDL2pp::Rect(duck_x, duck_y, TILE_SIZE, TILE_SIZE), 0.0, SDL2pp::NullOpt,
-                      flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);*/
+                      flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
         break;
-    case DuckStateType::IS_DEAD:
+    case DuckStateType::IS_DEAD: // fila 3, col 1 y 2
         std::cout << "Muere "  << "\n";
-
+        renderer.Copy(this->texture,
+                      SDL2pp::Rect(DUCK_IDLE_X + (SIZE_DUCK_SPRITE * frame), 
+                                    DUCK_IDLE_Y + (SIZE_DUCK_SPRITE * 2), 
+                                    SIZE_DUCK_SPRITE, SIZE_DUCK_SPRITE),
+                      SDL2pp::Rect(duck_x, duck_y, TILE_SIZE, TILE_SIZE), 0.0, SDL2pp::NullOpt,
+                      flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
         break;
     case DuckStateType::IS_PLANNING:
         std::cout << "Planea "  << "\n";
@@ -71,12 +83,16 @@ void DrawerPlayer::draw(SDL2pp::Renderer& renderer, const player_t& player) {
     case DuckStateType::IS_RECOILING:
         std::cout << "recoiling? "  << "\n";
         break;
-    case DuckStateType::IS_SLIPPING:
-        std::cout << "Se resbala "  << "\n";
+    case DuckStateType::IS_SLIPPING: // fila 3, col 1
+        renderer.Copy(this->texture,
+                      SDL2pp::Rect(DUCK_IDLE_X + (SIZE_DUCK_SPRITE * 0), 
+                                    DUCK_IDLE_Y + (SIZE_DUCK_SPRITE * 2), 
+                                    SIZE_DUCK_SPRITE, SIZE_DUCK_SPRITE),
+                      SDL2pp::Rect(duck_x, duck_y, TILE_SIZE, TILE_SIZE), 0.0, SDL2pp::NullOpt,
+                      flip ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
         break;
     case DuckStateType::IS_RUNNING:
         std::cout << "frame: " << frame << "\n";
-
         renderer.Copy(this->texture,
                       SDL2pp::Rect(DUCK_IDLE_X + (SIZE_DUCK_SPRITE * frame), 
                                     DUCK_IDLE_Y + (SIZE_DUCK_SPRITE * frame), 
