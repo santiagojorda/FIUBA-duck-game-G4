0fd6d04506b796a776bcddab337d845782e10a80
Santiago Jorda <sjorda@fi.uba.ar>
2024-11-27 10:26:13 -0300

    arreglo de caida luego de salto pato

--- server/game/game_physics.cpp
+++ server/game/game_physics.cpp
@@ -7,8 +7,8 @@ GamePhysics::GamePhysics(Map& _map): map(_map) {}
 
 bool GamePhysics::collision(const Rectangle& a, const Rectangle& b) {
     // esto solo funciona para rectangulos en 2d no rotados
-    return !(a.get_x_max() < (b.get_x_min() - 1) || a.get_x_min() > b.get_x_max() ||
-             a.get_y_max() < (b.get_y_min() - 1) || a.get_y_min() > b.get_y_max());
+    return !(a.get_x_max() < (b.get_x_min()) || a.get_x_min() > b.get_x_max() ||
+             a.get_y_max() < (b.get_y_min()) || a.get_y_min() > b.get_y_max());
 }
 
 void GamePhysics::falling(Positionable& target, uint iter_frame) {
